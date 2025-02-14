50e1ec7995bccff89bf9a0f4748b0372c806d06c
Th3Heretic <d.snow7324@student.leedsbeckett.ac.uk>
2025-02-14 01:02:40 +0000

    Initial Commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# bruteforce_fuzzer
-A program for bruteforcing hashes primarily on web SessionIDs as a personal project to aid my university studies
--- input_generator.py
+++
@@ -1,6 +0,0 @@
-import random
-import string
-
-def generate_random_string(length=10):
-    """Generates a random alphanumeric string of given length."""
-    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
